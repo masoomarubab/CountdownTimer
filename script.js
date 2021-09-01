2ac45880904598297b5497562e367a63f12c0c30
masoomarubab <rubabmasooma26@gmail.com>
2021-09-01 20:18:16 +0500

    Updating

--- script.js
+++ script.js
@@ -20,25 +20,3 @@ let updateCountdown = setInterval(() => {
         alert("Launched!");
     }
 },1000)
-
-function rotateDiv(i) {             // Flips the timers by manipulating classes.
-    $(i + ".back").removeClass("moveBack");
-    $(i + ".front").addClass("moveFront");
-    setTimeout(function() {
-      $(i + ".back").addClass("moveBack");
-      $(i + ".front").removeClass("moveFront");
-    }, 250);
-  };
-
-  setInterval(function() {            // function to flip the timers according to the time.
-    rotateDiv(".secs")
-    if ($(".secs").html() === "0") {
-      rotateDiv(".mins");
-    }
-    if ($(".mins").html() === "0" && $(".secs").html() === "0") {
-      rotateDiv(".hour");
-    }
-    if ($(".hours").html() === "0" && $(".mins").html() === "0" && $(".secz").html() === "0") {
-      rotateDiv(".day");
-    }
-  }, 1000);
